cf7b91ea6a6c9cff85c8eeef8e605fed07f73b68
Faryance <45324827+Faryance@users.noreply.github.com>
2021-04-07 13:08:39 +0800

    Removed test web

--- main.js
+++ main.js
@@ -2,11 +2,7 @@
 const request = require('request');
 const discord = require('discord.js');
 const client = new discord.Client();
-const express = require('express')
-const app = express()
-const port = 3000
 //const config = require('./config.json')
-// link = https://discord.com/api/oauth2/authorize?client_id=799179572108853279&permissions=7168&scope=bot
 
 //let api = config.api;
 //let token = config.token;
