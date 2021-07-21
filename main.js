eaee79eb398b054d6ab30c75f5f7e7af9423ad37
JashliXD <45324827+JashliXD@users.noreply.github.com>
2021-07-21 23:47:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 const request = require('request');
 const discord = require('discord.js');
-const intents = ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"];
+const intents = ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "DIRECT_MESSAGES"];
 const client = new discord.Client({intents: intents, ws:{intents: intents}});
 //const dotenv = require('dotenv').config()
 
